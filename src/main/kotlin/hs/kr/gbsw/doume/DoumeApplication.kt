178502d8eed790f3104eeeb23911ade617f3fca7
Boseok Son <dya_only@naver.com>
2025-03-08 01:03:25 +0900

    feat: initialize

---
+++ src/main/kotlin/hs/kr/gbsw/doume/DoumeApplication.kt
@@ -0,0 +1,11 @@
+package hs.kr.gbsw.doume
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoumeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoumeApplication>(*args)
+}
---
+++ src/test/kotlin/hs/kr/gbsw/doume/DoumeApplicationTests.kt
@@ -0,0 +1,13 @@
+package hs.kr.gbsw.doume
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoumeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

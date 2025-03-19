f038dd74a5476cda7754d42fc69807cb6c5777c5
YoungjinKim <talkingjuly@gmail.com>
2025-03-19 10:43:53 +0900

    refactor: Country.kt UserSignupRequest 변경 사항  - country의 valid 어노테이션을@NotBlank에서 @NotNull로 수정함.  - password의 조건 중 영문 대, 소문자 모두 최소 하나씩 포함하는 것에서 대, 소문자의 구분을 없앰.  - email 검증을 정규 표현식에서 @Email 어노테이션으로 수정함.

--- src/main/kotlin/hs/kr/gbsw/doumi/user/dto/UserDto.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/user/dto/UserDto.kt
@@ -1,23 +1,23 @@
 package hs.kr.gbsw.doumi.user.dto
 
 import hs.kr.gbsw.doumi.user.model.Users
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 
 data class UserSignupRequest(
-    @field:Pattern(
-        regexp = "^[0-9a-zA-Z]([-_\\\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\\\.]?[0-9a-zA-Z])*\\\\.[a-zA-Z]{2,3}\$",
-        message = "유효한 이메일 형식을 사용해주세요.")
+    @field:Email(message = "유효한 이메일 형식을 사용해주세요.")
     @field:NotBlank(message = "이메일을 입력해 주세요.")
     val email: String?,
 
     @field:Pattern(
-        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$",
+        regexp = "(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}",
         message = "비밀번호는 영문, 숫자 및 특수문자(@$!%*?&#)를 포함하여 8자 이상으로 작성해주세요.")
     @field:NotBlank(message = "비밀번호를 입력해 주세요.")
     val password: String?,
 
-    @field:NotBlank(message = "국적을 선택해 주세요.")
+    @field:NotNull(message = "국적을 선택해 주세요.")
     var country: Int?
 ) {
     fun toEntity(password: String): Users =
@@ -25,9 +25,7 @@ data class UserSignupRequest(
 }
 
 data class UserLoginRequest(
-    @field:Pattern(
-        regexp = "^[0-9a-zA-Z]([-_\\\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\\\.]?[0-9a-zA-Z])*\\\\.[a-zA-Z]{2,3}\$",
-        message = "유효한 이메일 형식을 사용해주세요.")
+    @field:Email(message = "유효한 이메일 형식을 사용해주세요.")
     @field:NotBlank(message = "이메일을 입력해 주세요.")
     val email: String,
     

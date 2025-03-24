886ace377c79b62afb01215f098ebf739c9235ae
Yunseong <jyerd333@gmail.com>
2025-03-24 16:05:00 +0900

    build: Add SMTP dependencies
    변경 사항
    - 이메일 전송 서비스를 구현하기 위한 의존성 추가
    
    기타 참고 사항
    - 관련 이슈: #5

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+	implementation("org.springframework.boot:spring-boot-starter-mail:3.1.5")
 	implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
 }
 

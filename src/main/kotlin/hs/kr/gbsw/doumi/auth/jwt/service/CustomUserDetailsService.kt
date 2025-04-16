1ad7e863e4fa9092544c6b34bc002a9ce36cd21b
Yunseong <jyerd333@gmail.com>
2025-04-16 19:34:42 +0900

    fix: Handle null password in createUserDetails function
    변경 사항
    - `createUserDetails` 함수에서 `user.password`가 `null`일 경우 빈 문자열(`""`)로 처리
    - `null`로 인한 오류 발생 문제 해결
    
    기타 참고 사항
    - 관련 이슈: 없음

--- src/main/kotlin/hs/kr/gbsw/doumi/auth/jwt/service/CustomUserDetailsService.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/auth/jwt/service/CustomUserDetailsService.kt
@@ -12,27 +12,19 @@ import org.springframework.stereotype.Service
 @Service
 class CustomUserDetailsService(
     private val userRepository: UserRepository,
-) : UserDetailsService {
+): UserDetailsService {
 
     override fun loadUserByUsername(username: String): UserDetails =
         userRepository.findByEmail(username)?.let {
             createUserDetails(it)
         } ?: throw UsernameNotFoundException("해당 유저는 존재하지 않습니다.")
 
-    private fun createUserDetails(user: Users): UserDetails {
-        val authorities = mutableListOf<SimpleGrantedAuthority>()
-
-        // 사용자 권한을 설정. 예를 들어 OAuth 사용자의 경우 적절한 권한 부여.
-        authorities.add(SimpleGrantedAuthority("ROLE_USER"))
-        if (user.provider != null) {
-            authorities.add(SimpleGrantedAuthority("ROLE_OAUTH"))
-        }
-
-        return CustomUser(
+    private fun createUserDetails(user: Users): UserDetails =
+        CustomUser(
             user.id!!,
             user.email,
-            user.password?: "",
-            authorities
+            user.password ?: "",
+            listOf(SimpleGrantedAuthority("USER"))
         )
-    }
+
 }

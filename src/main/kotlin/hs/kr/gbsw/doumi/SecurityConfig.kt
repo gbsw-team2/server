6f2c894e09d5ab394b503d83d78f76cc81116a46
YoungjinKim <talkingjuly@gmail.com>
2025-06-10 13:47:52 +0900

    refactor: allowedOrigin to allowedOriginPatterns by CorsConfigurationSource - Cors 설정에서 configuration.allowedOrigins를 .allowedOriginPatterns로 수정하였음

--- src/main/kotlin/hs/kr/gbsw/doumi/SecurityConfig.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/SecurityConfig.kt
@@ -54,7 +54,7 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
 
-        configuration.allowedOrigins = listOf("*")
+        configuration.allowedOriginPatterns = listOf("*")
         configuration.allowedMethods = listOf("POST", "GET", "PUT", "DELETE")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true

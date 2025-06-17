6dd85982de8cb8365dba1389b0abb98292545e90
YoungjinKim <talkingjuly@gmail.com>
2025-06-18 00:24:58 +0900

    hotfix: SecurityConfig - 계속해서 발생하는 Cors 오류를 해결하기 위한 수정 - allowedOrigins를 새로운 엔드포인트로 변경.

--- src/main/kotlin/hs/kr/gbsw/doumi/SecurityConfig.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/SecurityConfig.kt
@@ -55,7 +55,7 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
 
-        configuration.allowedOrigins = listOf("ttp://2025-322-doumi-nlb-fdc177bdc385739e.elb.ap-northeast-2.amazonaws.com")
+        configuration.allowedOrigins = listOf(" https://api.gbsw-doumi.kro.kr/")
         configuration.allowedMethods = listOf("POST", "GET", "PUT", "DELETE", "OPTIONS")
         configuration.allowedHeaders = listOf("Authorization", "Content-Type", "X-Requested-With")
         configuration.exposedHeaders = listOf("Authorization", "Content-Type")

982850c7df59b33fa6955a5f82790121e84189ea
YoungjinKim <talkingjuly@gmail.com>
2025-06-20 01:55:23 +0900

    refactor: 음성 번역 - 음성 파일을 RequestBody 대신 RequestPart로 받아옴 - ResponseEntity 반환 방식 변경

--- src/main/kotlin/hs/kr/gbsw/doumi/translate/controller/TranslateController.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/translate/controller/TranslateController.kt
@@ -7,10 +7,8 @@ import hs.kr.gbsw.doumi.translate.dto.VoiceResponse
 import hs.kr.gbsw.doumi.translate.service.TranslateService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RequestMapping("/api/translate")
 @RestController
@@ -20,25 +18,26 @@ class TranslateController(
 
     @PostMapping("/voice")
     fun uploadVoice(
+        @RequestPart audio: MultipartFile,
         @RequestBody dto: VoiceRequest
-    ): ResponseEntity<VoiceResponse> {
-        val text = translateService.toText(dto.audio!!, dto.beforeLang!!) ?:
-            return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+    ): ResponseEntity<String> {
+        val text = translateService.toText(audio, dto.beforeLang!!) ?:
+        return ResponseEntity(null, HttpStatus.BAD_REQUEST)
 
         val translated = translateService.translate(text, dto.beforeLang, dto.afterLang!!) ?:
-            return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+        return ResponseEntity.badRequest().body("음성 변환 실패.")
 
-        return ResponseEntity(VoiceResponse(translated), HttpStatus.OK)
+        return ResponseEntity.ok().body(translated)
     }
 
     @PostMapping("/text")
     fun uploadText(
         @RequestBody dto: TextRequest
-    ): ResponseEntity<TextResponse> {
+    ): ResponseEntity<String> {
         val translated = translateService.translate(dto) ?:
-            return ResponseEntity(null, HttpStatus.BAD_REQUEST)
+        return ResponseEntity.badRequest().body("음성 변환 실패.")
 
-        return ResponseEntity(TextResponse(translated), HttpStatus.OK)
+        return ResponseEntity.ok().body(translated)
     }
 
 }

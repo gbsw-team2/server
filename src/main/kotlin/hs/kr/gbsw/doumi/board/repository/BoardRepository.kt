67250bf30715e6eebd79dccfa1181c9a07989dcb
YoungjinKim <talkingjuly@gmail.com>
2025-06-24 16:51:42 +0900

    hotfix: get post list

--- src/main/kotlin/hs/kr/gbsw/doumi/board/repository/BoardRepository.kt
+++ src/main/kotlin/hs/kr/gbsw/doumi/board/repository/BoardRepository.kt
@@ -1,9 +1,6 @@
 package hs.kr.gbsw.doumi.board.repository
 
 import hs.kr.gbsw.doumi.board.model.Post
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
